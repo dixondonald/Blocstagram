fb3f13157cd8c27c3ea19f267b5f827db6f16150
Donald Dixon <dixondonald@gmail.com>
2016-02-04 14:15:30 -0500

    Checkpoint 27 Rebuilding Instagram: completed

--- Blocstagram/Blocstagram/AppDelegate.m
+++ Blocstagram/Blocstagram/AppDelegate.m
@@ -7,10 +7,9 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
+#import "ImagesTableViewController.h"
 
 @interface AppDelegate ()
-
 @end
 
 @implementation AppDelegate
@@ -18,13 +17,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ImagesTableViewController alloc] init]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
-    
     [self.window makeKeyAndVisible];
-    return YES;
-}
+    return YES;}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
