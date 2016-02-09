fa0e2dc6872f5fe171a9762b5fd47eca1ebbd455
Donald Dixon <dixondonald@gmail.com>
2016-02-09 16:26:48 -0500

    Assignment 28 completed

--- Blocstagram/Blocstagram/ImagesTableViewController.m
+++ Blocstagram/Blocstagram/ImagesTableViewController.m
@@ -13,7 +13,6 @@
 #import "Comment.h"
 
 @interface ImagesTableViewController ()
-
 @end
 
 @implementation ImagesTableViewController
@@ -50,10 +49,19 @@
 #pragma mark - Table view data source
 
 
+- (NSArray *) items {
+    
+    NSArray *items = [DataSource sharedInstance].mediaItems;
+                      
+    return items;
+}
+
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 
-    return [DataSource sharedInstance].mediaItems.count;
+    return self.items.count;
 }
+
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
@@ -81,7 +89,7 @@
         [cell.contentView addSubview:imageView];
     }
     
-    Media *item = [DataSource sharedInstance].mediaItems[indexPath.row];
+    Media *item = self.items[indexPath.row];
     imageView.image = item.image;
     
     return cell;
@@ -89,7 +97,7 @@
 
 - (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
 
-    Media *item = [DataSource sharedInstance].mediaItems[indexPath.row];
+    Media *item = self.items[indexPath.row];
     UIImage *image = item.image;
     
     return (CGRectGetWidth(self.view.frame) / image.size.width) * image.size.height;
