46f27d1386328cf9350ba36b21977a94cc9c8116
Donald Dixon <dixondonald@gmail.com>
2016-02-18 15:59:13 -0500

    Checkpoint 33 logging in: completed

---
+++ Blocstagram/Blocstagram/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  Blocstagram
+//
+//  Created by Ziggy on 2/18/16.
+//  Copyright © 2016 Ziggy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+@end
