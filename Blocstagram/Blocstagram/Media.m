d9b6640a2b0060de1271e6c387bc2e3e3ff226e8
Donald Dixon <dixondonald@gmail.com>
2016-02-23 17:03:43 -0500

    Checkpoint 35 data persistance: completed

--- Blocstagram/Blocstagram/Media.m
+++ Blocstagram/Blocstagram/Media.m
@@ -17,6 +17,7 @@
     
     if (self) {
         self.idNumber = mediaDictionary[@"id"];
+        NSLog(@"mediaItem ID: %@",self.idNumber);
         self.user = [[User alloc] initWithDictionary:mediaDictionary[@"user"]];
         NSString *standardResolutionImageURLString = mediaDictionary[@"images"][@"standard_resolution"][@"url"];
         NSURL *standardResolutionImageURL = [NSURL URLWithString:standardResolutionImageURLString];
@@ -45,6 +46,32 @@
     }
     
     return self;
+}
+
+#pragma mark - NSCoding
+
+- (instancetype) initWithCoder:(NSCoder *)aDecoder {
+    self = [super init];
+    
+    if (self) {
+        self.idNumber = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(idNumber))];
+        self.user = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(user))];
+        self.mediaURL = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(mediaURL))];
+        self.image = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(image))];
+        self.caption = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(caption))];
+        self.comments = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(comments))];
+    }
+    
+    return self;
+}
+
+- (void) encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeObject:self.idNumber forKey:NSStringFromSelector(@selector(idNumber))];
+    [aCoder encodeObject:self.user forKey:NSStringFromSelector(@selector(user))];
+    [aCoder encodeObject:self.mediaURL forKey:NSStringFromSelector(@selector(mediaURL))];
+    [aCoder encodeObject:self.image forKey:NSStringFromSelector(@selector(image))];
+    [aCoder encodeObject:self.caption forKey:NSStringFromSelector(@selector(caption))];
+    [aCoder encodeObject:self.comments forKey:NSStringFromSelector(@selector(comments))];
 }
 
 @end
