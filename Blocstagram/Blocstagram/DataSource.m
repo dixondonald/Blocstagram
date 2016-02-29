19c494c343593d0d448bb3c1179c6e90f44fa0c6
Donald Dixon <dixondonald@gmail.com>
2016-02-29 17:11:12 -0500

    Checkpoint-37

--- Blocstagram/Blocstagram/DataSource.m
+++ Blocstagram/Blocstagram/DataSource.m
@@ -17,6 +17,7 @@
 @interface DataSource () {
     NSMutableArray *_mediaItems;
 }
+
 @property (nonatomic, strong) NSArray *mediaItems;
 @property (nonatomic, assign) BOOL isRefreshing;
 @property (nonatomic, assign) BOOL isLoadingOlderItems;
@@ -33,28 +34,81 @@
     return @"ac6921d2952f4140968635727471fb4d";
 }
 
+
++ (instancetype) sharedInstance {
+    static dispatch_once_t once;
+    static id sharedInstance;
+    dispatch_once(&once, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+
+- (instancetype) init {
+    self = [super init];
+    
+    if (self) {
+        [self createOperationManager];
+        
+        self.accessToken = [UICKeyChainStore stringForKey:@"access token"];
+        
+        if (!self.accessToken) {
+            [self registerForAccessTokenNotification];
+        } else {
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                NSString *fullPath = [self pathForFilename:NSStringFromSelector(@selector(mediaItems))];
+                NSArray *storedMediaItems = [NSKeyedUnarchiver unarchiveObjectWithFile:fullPath];
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (storedMediaItems.count > 0) {
+                        NSMutableArray *mutableMediaItems = [storedMediaItems mutableCopy];
+                        
+                        [self willChangeValueForKey:@"mediaItems"];
+                        self.mediaItems = mutableMediaItems;
+                        [self didChangeValueForKey:@"mediaItems"];
+                        // #1
+                        for (Media* mediaItem in self.mediaItems) {
+                            [self downloadImageForMediaItem:mediaItem];
+                        }
+                        
+                    } else {
+                        [self populateDataWithParameters:nil completionHandler:nil];
+                    }
+                });
+            });        }
+    }
+    
+    return self;
+}
+
+- (void) registerForAccessTokenNotification {
+    [[NSNotificationCenter defaultCenter] addObserverForName:LoginViewControllerDidGetAccessTokenNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+        self.accessToken = note.object;
+        [UICKeyChainStore setString:self.accessToken forKey:@"access token"];
+        
+        // Got a token; populate the initial data
+        [self populateDataWithParameters:nil completionHandler:nil];
+    }];
+}
+
 - (void) populateDataWithParameters:(NSDictionary *)parameters completionHandler:(NewItemCompletionBlock)completionHandler {
     if (self.accessToken) {
-        // only try to get the data if there's an access token
         NSMutableDictionary *mutableParameters = [@{@"access_token": self.accessToken} mutableCopy];
-        
         [mutableParameters addEntriesFromDictionary:parameters];
         
-        [self.instagramOperationManager GET:@"users/self/feed"
-                                 parameters:mutableParameters
-                                    success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                                        if ([responseObject isKindOfClass:[NSDictionary class]]) {
-                                            [self parseDataFromFeedDictionary:responseObject fromRequestWithParameters:parameters];
-                                        }
+        [self.instagramOperationManager GET:@"users/self/feed" parameters:mutableParameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            if ([responseObject isKindOfClass:[NSDictionary class]]) {
+                [self parseDataFromFeedDictionary:responseObject fromRequestWithParameters:parameters];
+            }
                                         
-                                        if (completionHandler) {
-                                            completionHandler(nil);
-                                        }
-                                    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                                        if (completionHandler) {
-                                            completionHandler(error);
-                                        }
-                                    }];
+            if (completionHandler) {
+                completionHandler(nil);
+            }
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            if (completionHandler) {
+                completionHandler(error);
+            }
+        }];
     }
 }
 
@@ -67,6 +121,7 @@
         Media *mediaItem = [[Media alloc] initWithDictionary:mediaDictionary];
         
         if (mediaItem) {
+            [tmpMediaItems addObject:mediaItem];
             [self downloadImageForMediaItem:mediaItem];
         }
     }
@@ -74,17 +129,13 @@
     NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
     
     if (parameters[@"min_id"]) {
-        // This was a pull-to-refresh request
         
         NSRange rangeOfIndexes = NSMakeRange(0, tmpMediaItems.count);
         NSIndexSet *indexSetOfNewObjects = [NSIndexSet indexSetWithIndexesInRange:rangeOfIndexes];
         
         [mutableArrayWithKVO insertObjects:tmpMediaItems atIndexes:indexSetOfNewObjects];
     } else if (parameters[@"max_id"]) {
-        // This was an infinite scroll request
-        
         if (tmpMediaItems.count == 0) {
-            // disable infinite scroll, since there are no more older messages
             self.thereAreNoMoreOlderMessages = YES;
         } else {
             [mutableArrayWithKVO addObjectsFromArray:tmpMediaItems];
@@ -183,6 +234,19 @@
     }
 }
 
+- (void) createOperationManager {
+    NSURL *baseURL = [NSURL URLWithString:@"https://api.instagram.com/v1/"];
+    self.instagramOperationManager = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:baseURL];
+    
+    AFJSONResponseSerializer *jsonSerializer = [AFJSONResponseSerializer serializer];
+    
+    AFImageResponseSerializer *imageSerializer = [AFImageResponseSerializer serializer];
+    imageSerializer.imageScale = 1.0;
+    
+    AFCompoundResponseSerializer *serializer = [AFCompoundResponseSerializer compoundSerializerWithResponseSerializers:@[jsonSerializer, imageSerializer]];
+    self.instagramOperationManager.responseSerializer = serializer;
+}
+
 #pragma mark - Key/Value Observing
 
 - (NSUInteger) countOfMediaItems {
@@ -212,75 +276,6 @@
 - (void) deleteMediaItem:(Media *)item {
     NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
     [mutableArrayWithKVO removeObject:item];
-}
-
-
-+ (instancetype) sharedInstance {
-    static dispatch_once_t once;
-    static id sharedInstance;
-    dispatch_once(&once, ^{
-        sharedInstance = [[self alloc] init];
-    });
-    return sharedInstance;
-}
-
-- (instancetype) init {
-    self = [super init];
-    
-    if (self) {
-        [self createOperationManager];
-
-        self.accessToken = [UICKeyChainStore stringForKey:@"access token"];
-        
-        if (!self.accessToken) {
-            [self registerForAccessTokenNotification];
-        } else {
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                NSString *fullPath = [self pathForFilename:NSStringFromSelector(@selector(mediaItems))];
-                NSArray *storedMediaItems = [NSKeyedUnarchiver unarchiveObjectWithFile:fullPath];
-                
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    if (storedMediaItems.count > 0) {
-                        NSMutableArray *mutableMediaItems = [storedMediaItems mutableCopy];
-                        
-                        [self willChangeValueForKey:@"mediaItems"];
-                        self.mediaItems = mutableMediaItems;
-                        [self didChangeValueForKey:@"mediaItems"];
-                        // #1
-                        for (Media* mediaItem in self.mediaItems) {
-                            [self downloadImageForMediaItem:mediaItem];
-                        }
-                        
-                    } else {
-                        [self populateDataWithParameters:nil completionHandler:nil];
-                    }
-                });
-            });        }
-    }
-    
-    return self;
-}
-- (void) createOperationManager {
-    NSURL *baseURL = [NSURL URLWithString:@"https://api.instagram.com/v1/"];
-    self.instagramOperationManager = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:baseURL];
-    
-    AFJSONResponseSerializer *jsonSerializer = [AFJSONResponseSerializer serializer];
-    
-    AFImageResponseSerializer *imageSerializer = [AFImageResponseSerializer serializer];
-    imageSerializer.imageScale = 1.0;
-    
-    AFCompoundResponseSerializer *serializer = [AFCompoundResponseSerializer compoundSerializerWithResponseSerializers:@[jsonSerializer, imageSerializer]];
-    self.instagramOperationManager.responseSerializer = serializer;
-}
-
-- (void) registerForAccessTokenNotification {
-    [[NSNotificationCenter defaultCenter] addObserverForName:LoginViewControllerDidGetAccessTokenNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
-        self.accessToken = note.object;
-        [UICKeyChainStore setString:self.accessToken forKey:@"access token"];
-        
-        // Got a token; populate the initial data
-        [self populateDataWithParameters:nil completionHandler:nil];
-    }];
 }
 
 - (NSString *) pathForFilename:(NSString *) filename {
