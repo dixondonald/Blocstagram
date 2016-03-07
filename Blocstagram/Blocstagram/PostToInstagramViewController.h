c5b9100a1fb87e460cec8e6f2b0b6c1993386ff7
Donald Dixon <dixondonald@gmail.com>
2016-03-07 10:57:52 -0500

    Checkpoint 43 filter and submit: completed

---
+++ Blocstagram/Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Ziggy on 3/7/16.
+//  Copyright © 2016 Ziggy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
